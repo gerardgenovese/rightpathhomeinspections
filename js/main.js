f32cb3eb339b14bf29224cd8131eb19123373e5b
Gerard <gmoneymaster24@yahoo.com>
2018-04-26 20:30:15 -0400

    first day slideshow/navbar/tri-template

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//ALLOW DIV ELEMENT TO MAINTAIN BUTTON HOVER COLOR
+function hover() {
+
+
+  let header = document.querySelector(".side-nav");
+  let btns = header.getElementsByClassName("side-nav__link");
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      let current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+};
+hover();
+
+
+
+$('.carousel').carousel({
+  interval: 10000
+})
