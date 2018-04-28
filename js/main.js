a040f457741c35f5c43a8bb657f5a49d9491b17a
Gerard <gmoneymaster24@yahoo.com>
2018-04-27 23:48:33 -0400

    about & home buyers page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 //ALLOW DIV ELEMENT TO MAINTAIN BUTTON HOVER COLOR
-function hover() {
+// function hover() {
 
 
-  let header = document.querySelector(".side-nav");
-  let btns = header.getElementsByClassName("side-nav__link");
-  for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      let current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  }
-};
-hover();
+//   let header = document.querySelector(".side-nav");
+//   let btns = header.getElementsByClassName("side-nav__link");
+//   for (let i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function() {
+//       let current = document.getElementsByClassName("active");
+//       current[0].className = current[0].className.replace(" active", "");
+//       this.className += " active";
+//     });
+//   }
+// };
+// hover();
 
 
 
